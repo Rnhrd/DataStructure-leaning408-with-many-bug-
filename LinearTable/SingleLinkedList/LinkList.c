e374cc903b4b1132a322d06b865cf99f4a11df32
Rnhrd <75779845+Rnhrd@users.noreply.github.com>
2023-04-12 13:26:38 +0800

    modified:   LinearTable/SingleLinkedList/LinkList.c 	modified:   LinearTable/SingleLinkedList/LinkList.exe

--- LinearTable/SingleLinkedList/LinkList.c
+++ LinearTable/SingleLinkedList/LinkList.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef int ElemType;
-
 typedef struct LNode{        //定义单链表结点类型
-    ElemType data;               //数据域
+    int data;               //数据域
     struct LNode *next;     //指针域
 }LNode, *LinkList;
 
