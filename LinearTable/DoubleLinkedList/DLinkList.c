327563cf78c8d34416291cf69af213e651a1d0ae
Rnhrd <75779845+Rnhrd@users.noreply.github.com>
2023-04-12 15:33:28 +0800

    modified:   LinearTable/DoubleLinkedList/DLinkList.c 	modified:   LinearTable/SingleLinkedList/LinkList.c

--- LinearTable/DoubleLinkedList/DLinkList.c
+++ LinearTable/DoubleLinkedList/DLinkList.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+typedef int ElemType;
+
+typedef struct LNode
+{
+    ElemType data;
+    struct LNode *prior,*next;   
+}LNode, *DLinkList;
+
+void InitList()
